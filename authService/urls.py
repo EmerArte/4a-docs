fe26a47b59e8856de8332b1ae68179789a5c69a3
Emer Arteaga <arteagaemer@gmail.com>
2021-11-18 19:10:05 -0500

    url

--- authService/urls.py
+++ authService/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
+from authApp import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('login/', TokenObtainPairView.as_view()),
+    path('refresh/', TokenRefreshView.as_view()),
+    path('verifyToken/', views.VerifyTokenView.as_view()),
+    path('user/', views.UserDetailView.as_view()),
+    path('user/<int:pk>/', views.UserDetailView.as_view()), 
 ]
