974e0746c6de3c6ffabb00f82df87ec4c4315d71
Emer Arteaga <arteagaemer@gmail.com>
2021-11-26 22:00:25 -0500

    Se creó el proyecto de Springboot

---
+++ springboot-ms-admin/src/main/java/com/ciclo4/msadminlibreria/MsAdminLibreriaApplication.java
@@ -0,0 +1,13 @@
+package com.ciclo4.msadminlibreria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAdminLibreriaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsAdminLibreriaApplication.class, args);
+    }
+
+}
---
+++ springboot-ms-admin/src/test/java/com/ciclo4/msadminlibreria/MsAdminLibreriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciclo4.msadminlibreria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAdminLibreriaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
